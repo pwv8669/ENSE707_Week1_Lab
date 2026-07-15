a5912a027bd7de204f2b8a86f8da2c093ce4cc2e
pwv8669 <pwv8669@autuni.ac.nz>
2026-07-15 12:51:10 +1200

    Created BankAccount.cs

---
+++ ENSE707_Week1_Lab/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿namespace ENSE_Week1_Lab;
+
+public class  BankAccount
+{
+    public string AccountHolder { get; set; }
+    public decimal Balance { get; private set; }
+
+    public BankAccount(string accountHolder, decimal openingBalance)
+    {
+        AccountHolder = accountHolder;
+        Balance = openingBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance = Balance + amount;
+    }
+
+    public bool Withdraw(decimal amount)
+    {
+        Balance = Balance - amount;
+        return true;
+    }
+
+    public decimal CalculateTransactionFee(decimal amount)
+    {
+        return amount * 0.02m;
+    }
+}
