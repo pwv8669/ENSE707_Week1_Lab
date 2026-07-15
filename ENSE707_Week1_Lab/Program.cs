5e6d0c4bbad588f7390e5feedb09f4412f81fec9
pwv8669 <pwv8669@autuni.ac.nz>
2026-07-15 12:53:59 +1200

    Updated Program.cs as per Activity 2

--- ENSE707_Week1_Lab/Program.cs
+++ ENSE707_Week1_Lab/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ENSE_Week1_Lab;
 
+BankAccount account = new BankAccount("Student User", 100);
+
+account.Deposit(50);
+account.Withdraw(30);
+
+Console.WriteLine($"Account holder: {account.AccountHolder}");
+Console.WriteLine($"Current balance: {account.Balance}");
+Console.WriteLine($"Fee on $100 transaction: {account.CalculateTransactionFee(100)}");
